22357372a1269cd7775d092aec0648e24988b2ec
Justin Gutter <jgutter7@gmail.com>
2026-07-08 11:20:57 -0500

    Removed key

--- app/src/main/java/com/example/compoundinggrowth/api/AlphaVantageRepository.kt
+++ app/src/main/java/com/example/compoundinggrowth/api/AlphaVantageRepository.kt
@@ -7,12 +7,12 @@ import java.util.UUID
 class AlphaVantageRepository (private val alphaVantageApi: AlphaVantageApi) {
 
     suspend fun fetchStockPrice(stockSymbol: String): AlphaVantageApi.GlobalQuoteResponse {
-        return alphaVantageApi.getStockPrice(symbol = stockSymbol, apiKey = "AQ6ES11K2AUUB2M5")
+        return alphaVantageApi.getStockPrice(symbol = stockSymbol, apiKey = "XXX")
     }
 
     suspend fun fetchMultipleDailyStockPrices(stockSymbol: List<String>): List<AlphaVantageApi.DailyQuoteResponse> {
         return stockSymbol.map { symbol ->
-            alphaVantageApi.getDailyStockPrices(symbol = symbol, apiKey = "AQ6ES11K2AUUB2M5")
+            alphaVantageApi.getDailyStockPrices(symbol = symbol, apiKey = "XXX")
         }
     }
 }
